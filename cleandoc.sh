31a463dea102a07b169eeab902c178a7f7812ec5
Nohemi Fenugreek <nohemife@ga-y1.msk.21-school.ru>
2024-01-20 00:45:57 +0300

    v1.0.0

--- cleandoc.sh
+++ cleandoc.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+echo del com.docker.docker
 rm -rf ~/Library/Containers/com.docker.docker
+sleep 3
+echo mkdir
 mkdir -p ~/goinfre/Docker/Data
+sleep 3
+echo link com.docker.docker
 ln -s ~/goinfre/Docker ~/Library/Containers/com.docker.docker
+sleep 3
+reset
