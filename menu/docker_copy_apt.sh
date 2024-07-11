24423cf9a029eff192b17390fe4c883247d74bda
nohemife <honemife@X550CC>
2024-07-11 14:11:43 +0300

    Thu, 11 Jul 2024 14:11:43 +0300

--- menu/docker_copy_apt.sh
+++ menu/docker_copy_apt.sh
@@ -6,3 +6,10 @@
     echo $GREEN"Copy Success"$RESET
     # break
 # }
+
+
+# Linux
+# sudo apt-get install -y xclip
+# alias pbcopy="xclip -sel clip"
+# pbcopy < ~/.ssh/id_rsa.pub
+# alias cpkey="pbcopy < ~/.ssh/id_rsa.pub"
