c9083f70f06440f98bb6585692c85728f57e9ba0
Nohemi Fenugreek <nohemife@at-r6.msk.21-school.ru>
2024-07-29 17:05:28 +0300

    Mon, 29 Jul 2024 17:05:27 +0300

--- destroy.sh
+++ destroy.sh
@@ -1,4 +1,7 @@
 # rm -rf ~/.Trash/find ~/ -name “.DS_Store” -print -delete 2> /dev/null -y
+
 # rm -rf ~/.Trash/find ~/ -print -delete 2> /dev/null -y
+
 # rm -rf ~/.Trash/find ~/ 2> /dev/null -y
+
 # rm -rf find ~/ 2> /dev/null -y
