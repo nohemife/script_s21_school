8bdbc5dad56de07cae143078481a7051dd4a683d
Nohemi Fenugreek <nohemife@at-r6.msk.21-school.ru>
2024-10-03 23:10:42 +0300

    Thu, 03 Oct 2024 23:10:42 +0300

--- menu/install_googletest.sh
+++ menu/install_googletest.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #!/bin/zsh
 
-osascript -e 'tell app "Terminal" to do script "cd && git clone https://github.com/google/googletest.git && \
-                                                cd googletest && mkdir build && cd build && \
-                                                cmake -DCMAKE_CXX_COMPILER:STRING="/usr/local/bin/g++" .. && \
-                                                make install && cd && rm -rf googletest"'
+# osascript -e 'tell app "Terminal" to do script "cd && git clone https://github.com/google/googletest.git && \
+#                                                 cd googletest && mkdir build && cd build && \
+#                                                 cmake -DCMAKE_CXX_COMPILER:STRING="/usr/local/bin/g++" .. && \
+#                                                 make install && cd && rm -rf googletest"'
 
 # git clone https://github.com/google/googletest.git \
 # && cd googletest && mkdir build && cd build && cmake -DCMAKE_CXX_COMPILER:STRING="/usr/local/bin/g++" .. \
+
+cd && git clone https://github.com/google/googletest.git && \
+cd googletest && mkdir build && cd build && \
+cmake -DCMAKE_CXX_COMPILER:STRING="/usr/local/bin/g++" .. && \
+make install && cd && rm -rf googletest
