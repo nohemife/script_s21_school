a2226e7b9575b24c6c04a12d3b66539cec0c3c42
Nohemi Fenugreek <nohemife@at-r6.msk.21-school.ru>
2025-03-20 22:16:31 +0300

    Thu, 20 Mar 2025 22:16:31 +0300

--- plugins/format_style.sh
+++ plugins/format_style.sh
@@ -4,15 +4,18 @@
 # -------------------------------------------------------------------------- format google
 
 function fixformat() {
+	clang-format --version
 	FIND_FILE=($(find . -type f \( -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "*.cc" \) -print0 | xargs -0 -n 1 | sed 's|./| |' | tr -d '\n'))
+	echo "---------------- FILE -----------------"
+	echo $FIND_FILE
 	echo "---------------- CHECK ----------------"
-	clang-format -style=google -n $FIND_FILE
-	sleep 1
+	clang-format -style=Google -n $FIND_FILE
+	sleep 10
 	reset
 	echo "----------------- FIX -----------------"
-	clang-format -style=google -i $FIND_FILE
+	clang-format -style=Google -i $FIND_FILE
 	echo "---------------- CHECK ----------------"
-	clang-format -style=google -n $FIND_FILE
+	clang-format -style=Google -n $FIND_FILE
 	echo "----------------- END -----------------"
 
 }
