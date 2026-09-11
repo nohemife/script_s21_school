63a64d67527b4832e0a070c93423e323982e4e97
Nohemi Fenugreek <nohemife@at-k1.msk.21-school.ru>
2026-09-11 20:52:42 +0300

    Add helpme, fix Makefile and alias

--- plugins/alias.sh
+++ plugins/alias.sh
@@ -11,6 +11,7 @@ alias fs="FixFormatIntensive"
 
 alias gsd="git switch develop"
 alias gsn="git switch $(whoami)"
+alias rst="git checkout ."
 
 alias info="system_libs_info"
 
@@ -27,4 +28,31 @@ function init() {
 		cp $HOME/script_s21_school/plugins/Makefile $WORK_DIR/src/Makefile
 		cd $WORK_DIR/src
 	fi
+}
+
+function helpme() {
+	echo "restart      - перезапуск скрипта bashrc"
+	echo "info         - информация об установленных библиотеках"
+	echo "getkey       - генерация ключа ssh"
+
+	echo "work         - переход в рабочию область проекта"
+	echo "staff        - при передачи этой команде репозитория, происходит его клонирование и открытие"
+	echo "init         - копирование makefile в дерикторию текущего проекта"
+
+	echo "gsd          - переключение на ветку develop"
+	echo "gsn          - переключение на ветку пользователя"
+	echo "rst          - восстановление проекта в исходное состояние из репозитория"
+
+	echo "cc           - запуск утилиты cppcheck"
+	echo "ff           - запуск clang-format со стилем Google-Core"
+	echo "fs           - запуск clang-format со стилем Google-Intensive" 
+
+	echo "s21_lint     - запуск утилиты s21_lint для поиска нарушений"
+	echo "s21_lint -с  - запуск утилиты s21_lint для очистки комментариев"
+	echo "keepalive    - запуск утилиты keepalive, предотвращающая разлогин"
+
+	echo "vdb          - копирования файлов для дебага с вопросом"
+	echo "vdb -f       - принудительно перезаписать файлов для дебага без вопросов"
+	echo "clown        - запуск скрипта генерации клоунов"
+	# echo "gclone     - при передачи этой команде репозитория, происходит его клонирование и открытие"
 }
